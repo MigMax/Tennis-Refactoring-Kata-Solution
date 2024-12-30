afe95d930b3a0075bac2a2b31d49c2abfccacb09
MigMax <maxence.martin.info@gmail.com>
2024-12-30 22:26:58 +0100

    Extract methods GetScoreByContext, GetTieScore, GetEndGameScore and GetRegularScore

--- csharp/Tennis/TennisGame7.cs
+++ csharp/Tennis/TennisGame7.cs
@@ -21,50 +21,70 @@ public class TennisGame7(string player1Name, string player2Name) : ITennisGame
     {
         string result = "Current score: ";
 
-        if (_player1Score == _player2Score)
-        {
-            // tie score
-            result += _player1Score switch
-            {
-                0 => "Love-All",
-                1 => "Fifteen-All",
-                2 => "Thirty-All",
-                _ => "Deuce"
-            };
-        }
-        else if (_player1Score >= 4 || _player2Score >= 4)
-        {
-            // end-game score
-            result += (_player1Score - _player2Score) switch
-            {
-                1 => $"Advantage {player1Name}",
-                -1 => $"Advantage {player2Name}",
-                >= 2 => $"Win for {player1Name}",
-                _ => $"Win for {player2Name}"
-            };
-        }
-        else
-        {
-            // regular score
-            result += _player1Score switch
-            {
-                0 => "Love",
-                1 => "Fifteen",
-                2 => "Thirty",
-                _ => "Forty"
-            };
-
-            result += "-";
-
-            result += _player2Score switch
-            {
-                0 => "Love",
-                1 => "Fifteen",
-                2 => "Thirty",
-                _ => "Forty"
-            };
-        }
+        result += GetScoreByContext();
 
         return result + ", enjoy your game!";
+    }
+
+    private string GetScoreByContext()
+    {
+        if (_player1Score == _player2Score)
+        {
+            return GetTieScore();
+        }
+
+        if (_player1Score >= 4 || _player2Score >= 4)
+        {
+            return GetEndGameScore();
+        }
+
+        return GetRegularScore();
+    }
+    
+    private string GetTieScore()
+    {
+        return _player1Score switch
+        {
+            0 => "Love-All",
+            1 => "Fifteen-All",
+            2 => "Thirty-All",
+            _ => "Deuce"
+        };
+    }
+    
+    private string GetEndGameScore()
+    {
+        return (_player1Score - _player2Score) switch
+        {
+            1 => $"Advantage {player1Name}",
+            -1 => $"Advantage {player2Name}",
+            >= 2 => $"Win for {player1Name}",
+            _ => $"Win for {player2Name}"
+        };
+    }
+    
+    private string GetRegularScore()
+    {
+        var result = string.Empty;
+        
+        result += _player1Score switch
+        {
+            0 => "Love",
+            1 => "Fifteen",
+            2 => "Thirty",
+            _ => "Forty"
+        };
+
+        result += "-";
+
+        result += _player2Score switch
+        {
+            0 => "Love",
+            1 => "Fifteen",
+            2 => "Thirty",
+            _ => "Forty"
+        };
+        
+        return result;
     }
 }
